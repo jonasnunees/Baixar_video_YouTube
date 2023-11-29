7db7cccffef27a6688ea3508904e72216a592d5e
jonasnunees <151948400+jonasnunees@users.noreply.github.com>
2023-11-29 13:24:39 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from pytube import YouTube
+from pathlib import Path
+import os
+from pywebio.input import *
+from pywebio.output import *
+
+
+def video_download():
+    while True:
+        video_link = input('Link do YouTube')
+        if video_link.split("//")[0] == "https:":
+            put_text('Fazendo download...'.title()).style('color: red; font-size: 50px')
+            video_url = YouTube(video_link)
+            video = video_url.streams.get_highest_resolution()
+            path_to_download = (r'C:\Users\Gabinete-PC02\Downloads')
+            video.download(path_to_download)
+            put_text('Download finalizado!'.title())
+            startfile(r'C:\\Users\\Gabinete-PC02\\Downloads')
+
+
+if __name__ == '__main__':
+    video_download()
